34f97b2e183b10c542d85de72f8477d144a89a00
imaad-ak <imaad.alikhan93@gmail.com>
2023-11-04 17:24:01 +0530

    Solved test issues

--- test/unit/test-deploy.js
+++ test/unit/test-deploy.js
@@ -2,15 +2,17 @@ const { ethers } = require("hardhat")
 const { expect, assert } = require("chai")
 
 describe("CPAMM", async function () {
-    let ERC20Factory,
+    var ERC20Factory,
         Token1,
         Token2,
         CPAMMObject,
         Token1Address,
         Token2Address,
-        CPAMMAddress
-    beforeEach(async function () {
-        const [deployer, other] = await ethers.getSigners()
+        CPAMMAddress,
+        deployer,
+        other
+    before(async function () {
+        ;[deployer, other] = await ethers.getSigners()
 
         ERC20Factory = await ethers.getContractFactory("ERC20")
 
@@ -33,42 +35,63 @@ describe("CPAMM", async function () {
         CPAMMAddress = CPAMMObject.getAddress()
     })
 
-    it("Should have right addresses of CPAMM tokens", async function () {
-        const tk1Address = await CPAMMObject.token0()
-        assert.equal(tk1Address, Token1Address)
-        const tk2Address = await CPAMMObject.token1()
-        assert.equal(tk2Address, Token2Address)
+    describe("Constructor", async function () {
+        it("Should have right addresses of CPAMM tokens", async function () {
+            const tk1Address = await CPAMMObject.token0()
+            assert.equal(tk1Address, Token1Address)
+            const tk2Address = await CPAMMObject.token1()
+            assert.equal(tk2Address, Token2Address)
+        })
+
+        it("Should have zero total supply", async function () {
+            //const [deployer, other] = await ethers.getSigners()
+
+            const totalShares = await CPAMMObject.totalSupply()
+            const LiquidityProviderShares = await CPAMMObject.balanceOf(
+                deployer,
+            )
+
+            const expectedValue = "0"
+            assert.equal(totalShares.toString(), expectedValue)
+            assert.equal(LiquidityProviderShares.toString(), "0")
+        })
     })
 
-    it("Should have zero total supply", async function () {
-        const [deployer, other] = await ethers.getSigners()
+    describe("addLiquidty", async function () {
+        it("Should be able to add liquidity", async function () {
+            //const [deployer, other] = await ethers.getSigners()
 
-        const totalShares = await CPAMMObject.totalSupply()
-        const LiquidityProviderShares = await CPAMMObject.balanceOf(deployer)
+            await Token1.approve(CPAMMAddress, "1000")
+            await Token2.approve(CPAMMAddress, "500")
 
-        const expectedValue = "0"
-        assert.equal(totalShares.toString(), expectedValue)
-        assert.equal(LiquidityProviderShares.toString(), "0")
+            await CPAMMObject.addLiquidity("1000", "500")
+            const Token1Reserve = await CPAMMObject.reserve0()
+            const Token2Reserve = await CPAMMObject.reserve1()
+            const LiquidityProviderShares = await CPAMMObject.balanceOf(
+                deployer,
+            )
+            const expectedValue1 = "1000"
+            const expectedValue2 = "500"
+
+            assert.equal(Token1Reserve.toString(), expectedValue1)
+            assert.equal(Token2Reserve.toString(), expectedValue2)
+            assert.isAbove(LiquidityProviderShares, 0)
+            console.log(LiquidityProviderShares)
+        })
     })
 
-    beforeEach(async function () {
-        await Token1.approve(CPAMMAddress, "1000")
-        await Token2.approve(CPAMMAddress, "500")
-    })
+    describe("swap", async function () {
+        it("Should swap one token for another", async function () {
+            await Token1.connect(other).approve(CPAMMAddress, "100")
+            const transactionResponse = await CPAMMObject.connect(other).swap(
+                Token1,
+                "100",
+            )
+            await transactionResponse.wait(1)
+            const SwapperToken2Balance = await Token2.balanceOf(other)
 
-    it("Should be able to add liquidity", async function () {
-        const [deployer, other] = await ethers.getSigners()
-
-        await CPAMMObject.addLiquidity("1000", "500")
-        const Token1Reserve = await CPAMMObject.reserve0()
-        const Token2Reserve = await CPAMMObject.reserve1()
-        const LiquidityProviderShares = await CPAMMObject.balanceOf(deployer)
-        const expectedValue1 = "1000"
-        const expectedValue2 = "500"
-
-        assert.equal(Token1Reserve.toString(), expectedValue1)
-        assert.equal(Token2Reserve.toString(), expectedValue2)
-        assert.isAbove(LiquidityProviderShares, 0)
-        console.log(LiquidityProviderShares)
+            assert.isAbove(Number(SwapperToken2Balance), 0)
+            console.log(SwapperToken2Balance)
+        })
     })
 })
